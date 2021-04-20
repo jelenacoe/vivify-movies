9ca4cb1e7e0833e5b02b68a59060c91af8111147
ivan-vukosavljevic <ivanvukosavljevic@ymail.com>
2021-04-20 20:54:20 +0200

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\MovieController;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/movies', [MovieController::class, 'index']);
+
+Route::get('/movies/create', [MovieController::class, 'create']);
+Route::post('/movies', [MovieController::class, 'store']);
+
+Route::get('/movies/{movie}', [MovieController::class, 'show'])->name('movie');
